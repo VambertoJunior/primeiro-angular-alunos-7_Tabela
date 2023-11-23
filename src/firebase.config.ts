d0ab2b3de94f10eaa5f70a9ae4fafbfdd5de48dc
junio <juniorwan2012@gmail.com>
2023-11-23 00:58:14 -0300

    Initial commit

---
+++ src/firebase.config.ts
@@ -0,0 +1,8 @@
+ export const firebaseConfig = {
+    apiKey: "AIzaSyDXeaFxQH5nfIIVhWdo6A2oDtFqWJwFODY",
+    authDomain: "social-ifpb-vamberto.firebaseapp.com",
+    projectId: "social-ifpb-vamberto",
+    storageBucket: "social-ifpb-vamberto.appspot.com",
+    messagingSenderId: "269785418634",
+    appId: "1:269785418634:web:e1ba686bb7dcf1c089d673"
+  };
